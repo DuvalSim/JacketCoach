82aba9ca607a8d24630bd1b52b539f0fc228e8e9
Simon D Laptop <sduval6@gatech.edu>
2021-03-08 19:28:03 -0500

    - Refactor Application as MainActivity that contains a FragmentContainer - Create Map Fragment - Set up Navigation Graph - Set up BottomNavBar

---
+++ app/src/main/java/com/example/jacketcoach/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.jacketcoach;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* NAVIGATION */
+
+        //Initialize Bottom Navigation View
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_map,R.id.navigation_chat,R.id.navigation_profile)
+                .build();
+        //Initialize navigation controller
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navView,navController);
+
+        /* NAVIGATION END */
+    }
+
+}
