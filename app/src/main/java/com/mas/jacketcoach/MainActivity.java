3288b0957083a9ad41155e7bfe959d261b53dbf8
Simon D Laptop <sduval6@gatech.edu>
2021-03-14 10:09:20 -0400

    - Toolbar in fragment - Location Search fragment & small camera update

--- app/src/main/java/com/mas/jacketcoach/MainActivity.java
+++ app/src/main/java/com/mas/jacketcoach/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mas.jacketcoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -32,10 +33,10 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavView = findViewById(R.id.bottomNav_view);
 
         //Initialize top bar
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(myToolbar);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+//        setSupportActionBar(myToolbar);
 
-        ActionBar actionBar = this.getSupportActionBar();
+//        ActionBar actionBar = this.getSupportActionBar();
 //        actionBar.setDisplayShowTitleEnabled(false);
 //        Initialize navigation controller
 
@@ -43,80 +44,16 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
 
-//        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
-
         NavigationUI.setupWithNavController(bottomNavView,navController);
 
         /* NAVIGATION END */
     }
 
-//    private SearchView.OnQueryTextListener textListener = new SearchView.OnQueryTextListener() {
-//        @Override
-//        public boolean onQueryTextSubmit(String query) {
-//            Log.d("NAVIGATION", "quesry is :" + query);
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean onQueryTextChange(String newText) {
-//            Log.d("NAVIGATION", "newText is :" + newText);
-//            return true;
-//        }
-//    };
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-
-        Log.d("NAVIGATION", "onCreateMenu");
-        inflater.inflate(R.menu.toolbar_menu, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView =
-                (SearchView) searchItem.getActionView();
-
-//        searchView.setIconifiedByDefault(false);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Log.d("NAVIGATION", "quesry is :" + query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                Log.d("NAVIGATION", "newText is :" + newText);
-                return false;
-            }
-        });
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("NAVIGATION", "TEST");
     }
 
-
-
-
-
-
-    // ToolBar actions
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                Log.d("NAVIGATION","setting");
-                return true;
-
-            case R.id.action_favorite:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
 
 }
--- app/src/main/java/com/mas/jacketcoach/MapsFragment.java
+++ app/src/main/java/com/mas/jacketcoach/MapsFragment.java
@@ -1,15 +1,23 @@
 package com.mas.jacketcoach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,21 +27,48 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.mas.jacketcoach.model.Event;
 import com.mas.jacketcoach.model.MarkerInfo;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback {
+public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback, Toolbar.OnMenuItemClickListener {
 
     private GoogleMap mGoogleMap;
     private FirebaseEvents eventsData;
     private Map<Marker, MarkerInfo> mMarkerMap = new HashMap<>();
+    private static int AUTOCOMPLETE_REQUEST_CODE = 1;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //want to receive menu related callbacks
+        setHasOptionsMenu(true);
+        String apiKey = getString(R.string.google_maps_key);
+
+        /**
+         * Initialize Places. For simplicity, the API key is hard-coded. In a production
+         * environment we recommend using a secure mechanism to manage API keys.
+         */
+        if (!Places.isInitialized()) {
+            Places.initialize(getContext(), apiKey);
+        }
+
+        PlacesClient placesClient = Places.createClient(getContext());
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -68,11 +103,6 @@ public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClic
         }
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.d("NAVIGATION", "activity created");
-    }
 
     @Nullable
     @Override
@@ -86,12 +116,18 @@ public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClic
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Toolbar myToolbar = (Toolbar) getView().findViewById(R.id.main_toolbar);
+        myToolbar.inflateMenu(R.menu.toolbar_menu);
+        myToolbar.setOnMenuItemClickListener(this);
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
     }
+
 
     @Override
     public void onPause() {
@@ -101,11 +137,6 @@ public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClic
 //        mapStateManager.saveMapState(mGoogleMap);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d("NAVIGATION", "on Resume");
-    }
 
     @Override
     public void onStop() {
@@ -120,5 +151,68 @@ public class MapsFragment extends Fragment implements GoogleMap.OnInfoWindowClic
         MarkerInfo markerInfo = mMarkerMap.get(marker);
         EventWindowMap event = new EventWindowMap(markerInfo);
         event.show(getParentFragmentManager(),"eventMap");
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Log.d("NAVIGATION","setting");
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+            case R.id.action_search:
+                onSearchCalled();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    public void onSearchCalled() {
+        // Set the fields to specify which types of place data to return.
+//        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG);
+        List<Place.Field> fields = Arrays.asList(Place.Field.NAME, Place.Field.LAT_LNG);
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)//.setCountry("NG") //NIGERIA
+                .build(getContext());
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        Status status = Autocomplete.getStatusFromIntent(data);
+        Log.i("Autocomplete : ", "status returned : " + status);
+
+        if(requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            Log.i("Autocomplete : ", "onActivityResults");
+            if (resultCode == AutocompleteActivity.RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i("Autocomplete : ", "Got place : " + place.getName() + " " + place.getLatLng());
+
+                CameraUpdate camUpdate = CameraUpdateFactory.newLatLng(place.getLatLng());
+                mGoogleMap.moveCamera(camUpdate);
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+//                Status status = Autocomplete.getStatusFromIntent(data);
+                Autocomplete.getStatusFromIntent(data);
+                Toast.makeText(getContext(), "Error: " + status.getStatusMessage(), Toast.LENGTH_LONG).show();
+                Log.e("Autocomplete", status.getStatusMessage());
+            } else if (resultCode == AutocompleteActivity.RESULT_CANCELED) {
+                Log.i("Autocomplete : ", "Operation canceled");
+            }
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 }
