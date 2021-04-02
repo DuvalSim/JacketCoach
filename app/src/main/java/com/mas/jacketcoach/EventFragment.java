6ddf08fcdc36be5622f85b955e1f80374a650a43
Ninon Vrignaud <ninon.vrignaud@epfedu.fr>
2021-04-02 19:00:15 +0200

    mock list myEvents

--- app/src/main/java/com/mas/jacketcoach/EventFragment.java
+++ app/src/main/java/com/mas/jacketcoach/EventFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
@@ -52,7 +54,19 @@ public class EventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        String[] myEvents = {"Event 1", "Event 2"};
+
+        ListView listView = (ListView) view.findViewById(R.id.listViewEvent);
+        ArrayAdapter<String> listViewAdpater = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1,
+                myEvents
+        );
+
+        listView.setAdapter(listViewAdpater);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        return view;
     }
 }
